90130be03e0e187b37a20a8ee02ac4a6d0eb25b6
Ben Gedi <bengedi@gmail.com>
2023-09-24 18:44:07 +0300

    add dark mode

--- main.js
+++ main.js
@@ -1,27 +1,43 @@
-import './style.css'
+import "./style.css";
 import loaderIcon from "./assets/svg/loader.svg";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-      <!-- Title -->
-      <h1>Gallery - Infinite Scroll</h1>
-      <!-- Loader -->
-      <div class="loader" id="loader">
-          <img src="${loaderIcon}" alt="Loading">
-      </div>
-      <!-- Image Container -->
-      <div class="image-container" id="image-container"></div>
-      <!-- Script -->
-  </div>
+document.querySelector("#app").innerHTML = `
+    <header class="main-header">
+        <!-- Title -->
+        <h1>Gallery - Infinite Scroll</h1>
+        <div class="toggleWrapper">
+            <input type="checkbox" class="dn" id="dn"/>
+            <label for="dn" class="toggle">
+                <span class="toggle__handler">
+                <span class="crater crater--1"></span>
+                <span class="crater crater--2"></span>
+                <span class="crater crater--3"></span>
+                </span>
+                <span class="star star--1"></span>
+                <span class="star star--2"></span>
+                <span class="star star--3"></span>
+                <span class="star star--4"></span>
+                <span class="star star--5"></span>
+                <span class="star star--6"></span>
+            </label>
+        </div>
+    </header>
+    <!-- Loader -->
+    <div class="loader" id="loader">
+        <img src="${loaderIcon}" alt="Loading">
+    </div>
+
+    <!-- Image Container -->
+    <div class="image-container" id="image-container"></div>
 `;
 
-const imageContainer = document.getElementById('image-container');
-const loader = document.getElementById('loader');
+const imageContainer = document.getElementById("image-container");
+const loader = document.getElementById("loader");
+const checkbox = document.querySelector("input[type=checkbox]");
 // Unsplash API
-let count = 5;
+let count = 8;
 
 const apiKey = import.meta.env.VITE_API_KEY;
-debugger;
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
 let ready = false;
@@ -52,24 +68,23 @@ function imageLoaded() {
 function displayPhotos() {
     imagesLoaded = 0;
     totalImages = photosArray.length;
-    photosArray.forEach(photo => {
-        const a = document.createElement('a');
+    photosArray.forEach((photo) => {
+    const a = document.createElement("a");
         setAttributes(a, {
             href: photo.links.html,
-            target: '_blank',
+            target: "_blank",
         });
-        const img = document.createElement('img');
+        const img = document.createElement("img");
         setAttributes(img, {
             src: photo.urls.regular,
             alt: photo.alt_description,
             title: photo.alt_description,
         });
         // Event Listener, check when each is finished loading
-        img.addEventListener('load', imageLoaded);
+        img.addEventListener("load", imageLoaded);
         a.appendChild(img);
         imageContainer.appendChild(a);
-
-    })
+    });
 }
 
 // Get photo from Unsplash API
@@ -83,13 +98,26 @@ async function getPhotos() {
 }
 
 // Check to see if scrolling near bottom of page, Load More Photos
-window.addEventListener('scroll', () => {
-    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready) {
+window.addEventListener("scroll", () => {
+    if (
+        window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 &&
+        ready
+    ) {
         ready = false;
         getPhotos();
     }
+});
+
+// Dark Mode
+checkbox.addEventListener("change", (e) => {
+    document.body.dataset.theme = e.target.checked ? "dark" : "light";
+    localStorage.setItem("theme", document.body.dataset.theme);
+});
+
+if (localStorage.getItem("theme") === "dark") {
+    document.body.dataset.theme = "dark";
+    checkbox.checked = true;
 }
-)
 
 // On Load
 getPhotos();
